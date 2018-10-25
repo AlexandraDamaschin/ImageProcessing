fce0c1350129a3e7e42cddf48e9b82572e7df910
AlexandraDamaschin <damaschin.alexandra.maria@gmail.com>
2018-10-25 11:10:46 +0300

    outlier done

--- ImProcC#/ComputerVision/MainForm.cs
+++ ImProcC#/ComputerVision/MainForm.cs
@@ -544,7 +544,6 @@ namespace ComputerVision
             { n, n*n ,n},
             { 1, n ,1}};
 
-
             for (int i = 1; i < workImage.Width - 1; i++)
             {
                 for (int j = 1; j < workImage.Height - 1; j++)
